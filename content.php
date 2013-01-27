1f4c6a97542cd35b562e894f5dc13db30a2d6d0d
Paige Ponzeka <paigepon@gmail.com>
2013-01-27 15:35:32 -0500

    fixing up the styling of the comments section

--- content.php
+++ content.php
@@ -17,15 +17,46 @@
 		<header class="entry-header">
 			<?php the_post_thumbnail(); ?>
 			<?php if ( is_single() ) : ?>
-			<h1 class="entry-title"><?php the_title(); ?></h1>
+			  <h2 class="entry-title"><?php the_title(); ?></h2>
 			<?php else : ?>
-			<h1 class="entry-title">
-				<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
-			</h1>
+			  <h2 class="entry-title">
+  				<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
+  			</h2>
+
 			<?php endif; // is_single() ?>
-			<?php if ( comments_open() ) : ?>
+
+			 <?php
+        $date = sprintf( '<a href="%1$s" title="%2$s" rel="bookmark"><time class="entry-date" datetime="%3$s">%2$s</time></a>',
+      		esc_url( get_permalink() ),
+      		esc_attr( get_the_time('l, F jS, Y') ),
+      		esc_attr( get_the_date( 'c' ) ),
+      		esc_html( get_the_date() )
+      	);
+
+      	$author = sprintf( '<span class="author vcard"><a class="url fn n" href="%1$s" title="%2$s" rel="author">%3$s</a></span>',
+      		esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
+      		esc_attr( sprintf( __( 'View all posts by %s', 'twentytwelve' ), get_the_author() ) ),
+      		get_the_author()
+      	);
+
+      	// Translators: used between list items, there is a space after the comma.
+      	$categories_list = get_the_category_list( __( ', ', 'twentytwelve' ) );
+
+      	// Translators: used between list items, there is a space after the comma.
+      	$tag_list = get_the_tag_list( '', __( ', ', 'twentytwelve' ) );
+        ?>
+        <div class="entry-author"> Written by <span class="entry-author-name"><?php echo $author ?> </span> on <span class="entry-date"><?php echo $date ?></span></div>
+
+
+  			<?php if($categories_list) : ?>
+  				<div class="entry-categories">
+  				  <?php echo $categories_list?>
+  				</div>
+  			<?php endif; // tag_list() ?>
+
+			  <?php if ( comments_open() ) : ?>
 				<div class="comments-link">
-					<?php comments_popup_link( '<span class="leave-reply"> <span class=" icon-comment"></span> ' . __( 'Comment' ) . '</span>', __( '1 Comment' ), __( '% Comments' ) ); ?>
+					<?php comments_popup_link( '<span class="leave-reply"> <span class=" icon-comment"></span> ' . __( 'Leave a Comment' ) . '</span>', __( '1 Comment' ), __( '% Comments' ) ); ?>
 				</div><!-- .comments-link -->
 			<?php endif; // comments_open() ?>
 		</header><!-- .entry-header -->
@@ -42,7 +73,11 @@
 		<?php endif; ?>
 
 		<footer class="entry-meta">
-			<?php twentytwelve_entry_meta(); ?>
+      <?php if($tag_list) : ?>
+				  <div class="entry-tags">
+  			      Tags: <?php echo $tag_list?>
+  			    <div>
+			<?php endif; // tag_list() ?>
 			<?php edit_post_link( __( 'Edit', 'twentytwelve' ), '<span class="edit-link">', '</span>' ); ?>
 			<?php if ( is_singular() && get_the_author_meta( 'description' ) && is_multi_author() ) : // If a user has filled out their description and this is a multi-author blog, show a bio on their entries. ?>
 				<div class="author-info">
--- functions.php
+++ functions.php
@@ -286,47 +286,53 @@ function port25_comment( $comment, $args, $depth ) {
 	?>
 	<li <?php comment_class(); ?> id="li-comment-<?php comment_ID(); ?>">
 		<article id="comment-<?php comment_ID(); ?>" class="comment">
-			<header class="comment-meta comment-author vcard">
-			  <div class="comment-author-image">
-			    <?php echo get_avatar( $comment, 44 ); ?>
-			  </div>
-			  <div class="comment-author-details">
-			    <div class="comment-author-name">
-			      <?php
+		  <div class="comment-sidebar">
+		    <div class="comment-author-avatar">
+		       <?php echo get_avatar( $comment, 44 ); ?>
+		    </div>
+        <div class="comment-timestamp">
+		       <?php
+			        	printf( '<a href="%1$s"><time datetime="%2$s">%3$s</time></a>',
+      						esc_url( get_comment_link( $comment->comment_ID ) ),
+      						get_comment_time( 'M j g:j a' ),
+      						/* translators: 1: date, 2: time */
+      						sprintf( __( '%1$s <br /> %2$s', 'twentytwelve' ), get_comment_date("M j"), get_comment_time("g:j a") )
+      					);
+			      ?>
+		    </div>
+
+		     <div class="comment-edit-link">
+  		      <?php edit_comment_link( __( 'Edit', 'twentytwelve' ), '<p class="edit-link">', '</p>' ); ?>
+  		    </div>
+
+
+		  </div>
+		  <div class="comment-body">
+		    <div class="comment-author-username">
+		       <?php
     					printf( '<cite class="fn">%1$s %2$s</cite>',
     						get_comment_author_link(),
     						// If current post author is also comment author, make it known visually.
     						( $comment->user_id === $post->post_author ) ? '<span> ' . __( 'Post author', 'twentytwelve' ) . '</span>' : ''
     					);
     					?>
-			    </div>
-			    <div class="comment-author-timestamp">
-			      <?php
-			        	printf( '<a href="%1$s"><time datetime="%2$s">%3$s</time></a>',
-      						esc_url( get_comment_link( $comment->comment_ID ) ),
-      						get_comment_time( 'c' ),
-      						/* translators: 1: date, 2: time */
-      						sprintf( __( '%1$s at %2$s', 'twentytwelve' ), get_comment_date(), get_comment_time() )
-      					);
-			      ?>
-			    </div>
+		    </div>
 
-			  </div>
+		    <div class="comment-content-moderation">
+		      	<?php if ( '0' == $comment->comment_approved ) : ?>
+      				<p class="comment-awaiting-moderation"><?php _e( 'Your comment is awaiting moderation.', 'twentytwelve' ); ?></p>
+      			<?php endif; ?>
+		    </div>
+		    <div class="comment-content">
+		      	<?php comment_text(); ?>
+		    </div>
 
-			</header><!-- .comment-meta -->
+		    <div class="comment-reply">
+		      <?php comment_reply_link( array_merge( $args, array( 'reply_text' => __( 'Reply', 'twentytwelve' ), 'after' => ' <span>&darr;</span>', 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ); ?>
 
-			<?php if ( '0' == $comment->comment_approved ) : ?>
-				<p class="comment-awaiting-moderation"><?php _e( 'Your comment is awaiting moderation.', 'twentytwelve' ); ?></p>
-			<?php endif; ?>
+		    </div>
+		  </div>
 
-			<section class="comment-content comment">
-				<?php comment_text(); ?>
-				<?php edit_comment_link( __( 'Edit', 'twentytwelve' ), '<p class="edit-link">', '</p>' ); ?>
-			</section><!-- .comment-content -->
-
-			<div class="reply">
-				<?php comment_reply_link( array_merge( $args, array( 'reply_text' => __( 'Reply', 'twentytwelve' ), 'after' => ' <span>&darr;</span>', 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ); ?>
-			</div><!-- .reply -->
 		</article><!-- #comment-## -->
 	<?php
 		break;
@@ -334,52 +340,6 @@ function port25_comment( $comment, $args, $depth ) {
 }
 endif;
 
-if ( ! function_exists( 'twentytwelve_entry_meta' ) ) :
-/**
- * Prints HTML with meta information for current post: categories, tags, permalink, author, and date.
- *
- * Create your own twentytwelve_entry_meta() to override in a child theme.
- *
- * @since Twenty Twelve 1.0
- */
-function twentytwelve_entry_meta() {
-	// Translators: used between list items, there is a space after the comma.
-	$categories_list = get_the_category_list( __( ', ', 'twentytwelve' ) );
-
-	// Translators: used between list items, there is a space after the comma.
-	$tag_list = get_the_tag_list( '', __( ', ', 'twentytwelve' ) );
-
-	$date = sprintf( '<a href="%1$s" title="%2$s" rel="bookmark"><time class="entry-date" datetime="%3$s">%4$s</time></a>',
-		esc_url( get_permalink() ),
-		esc_attr( get_the_time() ),
-		esc_attr( get_the_date( 'c' ) ),
-		esc_html( get_the_date() )
-	);
-
-	$author = sprintf( '<span class="author vcard"><a class="url fn n" href="%1$s" title="%2$s" rel="author">%3$s</a></span>',
-		esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
-		esc_attr( sprintf( __( 'View all posts by %s', 'twentytwelve' ), get_the_author() ) ),
-		get_the_author()
-	);
-
-	// Translators: 1 is category, 2 is tag, 3 is the date and 4 is the author's name.
-	if ( $tag_list ) {
-		$utility_text = __( 'This entry was posted in %1$s and tagged %2$s on %3$s<span class="by-author"> by %4$s</span>.', 'twentytwelve' );
-	} elseif ( $categories_list ) {
-		$utility_text = __( 'This entry was posted in %1$s on %3$s<span class="by-author"> by %4$s</span>.', 'twentytwelve' );
-	} else {
-		$utility_text = __( 'This entry was posted on %3$s<span class="by-author"> by %4$s</span>.', 'twentytwelve' );
-	}
-
-	printf(
-		$utility_text,
-		$categories_list,
-		$tag_list,
-		$date,
-		$author
-	);
-}
-endif;
 
 /**
  * Extends the default WordPress body class to denote:
